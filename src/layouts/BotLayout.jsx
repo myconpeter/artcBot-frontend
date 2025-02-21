78d1ec3319fd19e7784afa4a70d0b5952ecd0b16
Micheal Peter <79177019+myconpeter@users.noreply.github.com>
2025-02-21 19:28:24 +0100

    hmmmm

--- src/layouts/BotLayout.jsx
+++ src/layouts/BotLayout.jsx
@@ -7,36 +7,36 @@ import { NavLink, Outlet } from 'react-router-dom'
 const BotLayout = () => {
   return (
     <div className='min-h-screen w-[100%] flex items-center justify-center'>
-      <nav className='flex justify-around items-center fixed bottom-0 z-40 bg-[#06111c] w-[80%] h-[10%] pt-2 rounded-2xl'>
+      <nav className='flex justify-around items-center fixed bottom-0 z-40 bg-[#06111c] w-[80%] h-[8%] pt-2 rounded-2xl'>
         <NavLink to='mine'>
           {({ isActive }) => (
             <div className='flex flex-col items-center'>
-              <GoHomeFill className={`text-3xl ${isActive ? 'text-[#00D4FF]' : 'text-gray-400'}`} />
-              <p className={isActive ? 'text-white' : 'text-gray-400'}>Mine</p>
+              <GoHomeFill className={`text-2xl ${isActive ? 'text-[#00D4FF]' : 'text-gray-400'}`} />
+              <p className={isActive ? 'text-white text-sm' : 'text-gray-400 text-sm'}>Mine</p>
             </div>
           )}
         </NavLink>
         <NavLink to='task'>
           {({ isActive }) => (
             <div className='flex flex-col items-center'>
-              <FaTasks className={`text-3xl ${isActive ? 'text-[#00D4FF]' : 'text-gray-400'}`} />
-              <p className={isActive ? 'text-white' : 'text-gray-400'}>Task</p>
+              <FaTasks className={`text-2xl ${isActive ? 'text-[#00D4FF]' : 'text-gray-400'}`} />
+              <p className={isActive ? 'text-white text-sm' : 'text-gray-400 text-sm'}>Task</p>
             </div>
           )}
         </NavLink>
         <NavLink to='refer'>
           {({ isActive }) => (
             <div className='flex flex-col items-center'>
-              <MdPersonAddAlt1 className={`text-3xl ${isActive ? 'text-[#00D4FF]' : 'text-gray-400'}`} />
-              <p className={isActive ? 'text-white' : 'text-gray-400'}>Refer</p>
+              <MdPersonAddAlt1 className={`text-2xl ${isActive ? 'text-[#00D4FF]' : 'text-gray-400'}`} />
+              <p className={isActive ? 'text-white text-sm' : 'text-gray-400 text-sm'}>Friends</p>
             </div>
           )}
         </NavLink>
         <NavLink to='wallet'>
           {({ isActive }) => (
             <div className='flex flex-col items-center'>
-              <FaWallet className={`text-3xl ${isActive ? 'text-[#00D4FF]' : 'text-gray-400'}`} />
-              <p className={isActive ? 'text-white' : 'text-gray-400'}>Wallet</p>
+              <FaWallet className={`text-2xl ${isActive ? 'text-[#00D4FF]' : 'text-gray-400'}`} />
+              <p className={isActive ? 'text-white text-sm' : 'text-gray-400 text-sm'}>Wallet</p>
             </div>
           )}
         </NavLink>
